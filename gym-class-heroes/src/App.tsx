de38c01102ebeb9eaa1fa3c1790a86ad2979d776
Faith Hilarde <fsomo@academic.rrc.ca>
2026-01-15 10:51:30 -0600

    feat(app): Integrate header in app.tsx with project name and description.

--- gym-class-heroes/src/App.tsx
+++ gym-class-heroes/src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
 import Footer from './components/footer/footer';
+import {Header} from './components/header/header';
 
 function App() {
   return (
     <div>
+      <Header
+        projectName="Gym Class Heroes"
+        projectDescription="Workout Management System"/>
       <Footer names={["Faith Hilarde","Nikita Kurakin", "Hoang Son Nguyen"]} />
     </div>
   );
