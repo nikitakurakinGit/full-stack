307190414c627a7aeacfd186fff56055186d191e
Son Nguyen <hnguyen31@academic.rrc.ca>
2026-01-15 13:49:14 -0600

    Made the workout list and render in app

--- gym-class-heroes/src/App.tsx
+++ gym-class-heroes/src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css'
 import Footer from './components/footer/footer';
+import WorkoutList from './components/workouts/workouts';
 
 function App() {
   return (
     <div>
+      <WorkoutList 
+      title="Beginner Level"
+      workouts={["50 Push-ups", "50 Squats", "50 Sit-ups"]}/>
+      <WorkoutList 
+      title="Intermediate Level"
+      workouts={["100 Push-ups", "100 Squats", "100 Sit-ups"]}/>
+      <WorkoutList 
+      title="Advanced Level"
+      workouts={["150 Push-ups", "150 Squats", "150 Sit-ups"]}/>
+      
       <Footer names={["Faith Hilarde","Nikita Kurakin", "Hoang Son Nguyen"]} />
+            
     </div>
   );
 }
