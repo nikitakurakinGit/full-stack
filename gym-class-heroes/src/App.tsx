a87c58ffc01a627bbe2bfd55372e2cc4c5845c51
Faith Hilarde <fsomo@academic.rrc.ca>
2026-01-15 20:13:20 -0600

    Add athlete data and integrate AthleteList component into app.tsx.

--- gym-class-heroes/src/App.tsx
+++ gym-class-heroes/src/App.tsx
@@ -1,9 +1,21 @@
 import './App.css'
+import { AthleteList } from './components/athletes/athletes'; 
 import Footer from './components/footer/footer';
+import type { Athletes } from './components/interface/athletes';
+
 
 function App() {
+  const athlete: Athletes[] = [
+    { id: 1, name: "Hoang Son Nguyen", sport: "Soccer", experience: "Advanced", status: "Active" },
+    { id: 2, name: "Nikita Kurakin", sport: "Basketball", experience: "Intermediate", status: "Inactive" },
+    { id: 3, name: "Faith Hilarde", sport: "Wrestling", experience: "Beginner", status: "Injured" }
+  ]
+
   return (
     <div>
+
+      <AthleteList title="Athletes" athlete={athlete} />
+      
       <Footer names={["Faith Hilarde","Nikita Kurakin", "Hoang Son Nguyen"]} />
     </div>
   );
